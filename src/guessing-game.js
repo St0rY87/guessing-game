737ca1b5ba2edbb899a5c6f9011eb1e0e7db468c
Dmitriy Savin <mr.dima337@gmail.com>
2023-10-27 12:23:26 +0300

    feat: implement solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.solutionCandidate = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.solutionCandidate = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.solutionCandidate;
     }
-
+    
     greater() {
-
+        this.min = this.solutionCandidate;
     }
 }
 
